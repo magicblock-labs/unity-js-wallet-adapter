d58daf70d778eba6edf233a896aed7154dced206
Nicolas Francisquelo Tacca <nfrancisquelo2@gmail.com>
2023-04-18 15:21:37 +0200

    Renamed build to dist and removed from gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   mode: 'production',
   entry: ['./src/index.ts'],
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'wallet-adapter-lib.js',
   },
   plugins: [
