4350c0d2c96e8b879837d28a1b3c0fe8b3449159
Nicolas Francisquelo Tacca <nfrancisquelo2@gmail.com>
2023-04-23 13:00:28 +0200

    Wallet icons + Default Wallets (#2)
    * Added icons and default wallet adapters
    
    * Improved Wallet Adapter initalization

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import {
+    Adapter,
     WalletNotReadyError,
     WalletReadyState,
     isWalletAdapterCompatibleStandardWallet,
@@ -11,12 +12,36 @@ import {
 } from '@solana/web3.js';
 import { getWallets } from "@wallet-standard/app";
 import type { Wallet } from "@wallet-standard/base";
+import { PhantomWalletAdapter, SolflareWalletAdapter  } from '@solana/wallet-adapter-wallets';
+
+
+import { Canvg } from "canvg";
+
+const defaultWalletAdapters: Array<Adapter> = [
+    new PhantomWalletAdapter(),
+    new SolflareWalletAdapter(),
+];
+
+const { get, on } = getWallets();
+let walletAdapters : Array<Adapter> = getWalletAdapters();
+
+function getWalletAdapters () : Array<Adapter> {
+    let standardAdapters = wrapWalletsInAdapters(get());
+    let adapters =  defaultWalletAdapters.filter((adapter) => !standardAdapters.some((wallet) => wallet.name === adapter.name));
+    return [...adapters, ...standardAdapters] as Array<Adapter>;
+}
+
+on('register', (...wallets) => {
+    walletAdapters = [ ...walletAdapters, ...wrapWalletsInAdapters(wallets) ];
+});
+on('unregister', (...wallets) => {
+    walletAdapters = walletAdapters.filter((adapter) => wallets.some((wallet) => wallet.name === adapter.name));
+});
 
 
 
-let wallets : Array<StandardWalletAdapter> = [];
-
-initWallets();
+const canvas: HTMLCanvasElement = document.createElement('canvas');
+canvas.style.visibility = 'hidden';
 
 
 async function connect(adapter): Promise<any> {
@@ -95,16 +120,22 @@ declare global {
 interface WalletAdapterLibrary {
     connectWallet:  (walletName: string) => Promise<any>;
     signTransaction:  (walletName: string, transactionStr: string) => Promise<any>;
-    getWallets: () => string;
+    getWallets: () => Promise<any>;
     signMessage: (walletName: string, messageStr: string) => Promise<any>;
 }
 
+interface WalletData {
+    name: string;
+    installed: boolean;
+    icon: string;
+}
+
 function getWalletAdapterByName(walletName) {
-    let walletIndex = wallets.findIndex(x => x.name === walletName);
+    let walletIndex = walletAdapters.findIndex(x => x.name === walletName);
     if (walletIndex === -1) {
         return null;
     }
-    return wallets[walletIndex];
+    return walletAdapters[walletIndex];
 }
 
 async function connectWallet(walletName) {
@@ -129,32 +160,49 @@ async function signMessageWallet(walletName, messageStr) {
 
 
 function wrapWalletsInAdapters(
-    wallets: ReadonlyArray<Wallet>
+    wallets: ReadonlyArray<Wallet>,
   ): Array<StandardWalletAdapter> {
     return wallets
-      .filter(isWalletAdapterCompatibleStandardWallet )
-      .map((wallet) => new StandardWalletAdapter({ wallet }));
-  }
+        .filter(isWalletAdapterCompatibleStandardWallet)
+        .map((wallet) => new StandardWalletAdapter({wallet}));
+}
+    
 
-  function initWallets() {
-    const { get, on } = getWallets();
-    const walletsStandard = get();
-    wallets = wrapWalletsInAdapters(walletsStandard);
-    console.log(wallets);
-  }
-
-function getWalletsData() {
-    if (wallets.length === 0) {
-        initWallets();
+async function getWalletIconPng(iconDataUrl) {
+    let iconDataUrlPng: string;
+    // When the icon format is SVG, we need to convert it to PNG
+    if (iconDataUrl.startsWith('data:image/svg+xml')) {
+        const context = canvas.getContext('2d');
+        const svg = await Canvg.from(
+            context,
+            iconDataUrl
+          );
+        await svg.render();
+        iconDataUrlPng = canvas.toDataURL();
     }
-    const walletData = wallets.map(wallet => {
-        return {
+    else {
+        iconDataUrlPng = iconDataUrl;
+    }
+    // Remove Encoding prefix
+    return iconDataUrlPng.replace(/^data:image\/\w+;base64,/, '');
+}
+
+
+
+
+
+async function getWalletsData() {
+    let walletsData: Array<WalletData> = [];
+    
+    for (let wallet of walletAdapters) {
+        let icon = await getWalletIconPng(wallet.icon);
+        walletsData.push({
             name: wallet.name,
             installed: wallet.readyState == WalletReadyState.Installed,
-        }
+            icon: icon
+        });
     }
-    );
-    return JSON.stringify({wallets:walletData});
+    return JSON.stringify({wallets:walletsData});
 }
 
 const walletAdapterLib: WalletAdapterLibrary = {
