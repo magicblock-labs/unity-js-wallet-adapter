d4a228e6ea6961a58e53487a1e540b8ef7214abc
Gabriele Picco <piccogabriele@gmail.com>
2024-02-23 10:23:27 +0100

    :sparkles: Publish the library to npm (#7)
    * :bookmark: Bump version
    
    * :bug: Fix dist file path

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require("webpack");
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 module.exports = {
-  devtool: "source-map",
+  devtool: false,
   mode: "production",
   entry: ["./src/index.ts"],
   output: {
@@ -17,16 +17,6 @@ module.exports = {
     new webpack.ProvidePlugin({
       process: "process/browser",
     }),
-    //new webpack.HotModuleReplacementPlugin(),
-    //new webpack.NoErrorsPlugin(),
-    //new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    // new webpack.DefinePlugin({
-    //   'process.env': {
-    //     // This has effect on the react lib size
-    //     'NODE_ENV': JSON.stringify('production'),
-    //   }
-    // }),
-    // new webpack.optimize.UglifyJsPlugin(),
     new NodePolyfillPlugin(),
   ],
   module: {
@@ -47,6 +37,5 @@ module.exports = {
   },
   optimization: {
     minimize: true,
-    //minimizer: [new TerserPlugin()],
   },
 };
